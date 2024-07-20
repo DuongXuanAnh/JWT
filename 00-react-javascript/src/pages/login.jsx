8c66d4fc2fcd85b9da0560eb38f5bc8b2b82c9c7
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2024-07-20 16:40:26 +0200

    login done

--- 00-react-javascript/src/pages/login.jsx
+++ 00-react-javascript/src/pages/login.jsx
@@ -1,8 +1,90 @@
-import React from 'react'
+import React from 'react';
+import { Button, Checkbox, Form, Input, notification } from 'antd';
+import { loginApi } from '../util/api';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+
+  const navigate = useNavigate();
+
+  const onFinish = async (values) => {
+
+    const { email, password} = values;
+
+    const res = await loginApi( email, password);
+
+    
+    if(res && res.EC === 0){
+      localStorage.setItem("accessToken", res.accessToken);
+      notification.success({
+        message: 'Login success',
+        description: 'You have successfully login'
+      });
+      navigate('/');
+    }else{
+      notification.error({
+        message: 'Login failed',
+        description: res?.EM ?? "error"
+      });
+    }
+
+    console.log('Success:', res);
+  };
+
   return (
-    <div>LoginPage</div>
+    <div style={{margin: 50}}>
+       <Form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+  
+    onFinish={onFinish}
+    autoComplete="off"
+    layout="vertical"
+  >
+    <Form.Item
+      label="Email"
+      name="email"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your email!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+    
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input.Password />
+
+    </Form.Item>
+
+    <Form.Item>
+      <Button type="primary" htmlType="submit">
+        Login
+      </Button>
+    </Form.Item>
+  </Form>
+    </div>
+   
   )
 }
 
--- 00-react-javascript/src/util/api.js
+++ 00-react-javascript/src/util/api.js
@@ -7,4 +7,11 @@ const createUserApi = (name, email, password) => {
     return axios.post(URL_API, data);
 }
 
-export { createUserApi };
+const loginApi = (email, password) => {
+    const URL_API = "/v1/api/login";
+    const data = {email, password};
+
+    return axios.post(URL_API, data);
+}
+
+export { createUserApi, loginApi };
