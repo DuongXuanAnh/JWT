03f44085bd6942552499c9aa826c6b522243a1c5
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2024-07-22 15:48:13 +0200

    Handle F5 refresh + loading

--- 00-react-javascript/src/App.jsx
+++ 00-react-javascript/src/App.jsx
@@ -1,20 +1,56 @@
-import { useEffect } from 'react'
+import { useContext, useEffect } from 'react'
 import  Header  from './components/layout/header'
 import { Outlet } from 'react-router-dom'
+import { AuthContext } from './components/context/auth.context';
+import axios from './util/axios.customize';
+import { Spin } from 'antd';
 
 
 function App() {
 
+  const {setAuth, appLoading, setAppLoading} = useContext(AuthContext);
+
   useEffect(() => {
 
-    
+    setAppLoading(true);
+
+    const fetchAccount = async () => {
+        const res = await axios.get(`/v1/api/account`);
+        if(res){
+            setAuth({
+                isAuthenticated: true,
+                user: {
+                    email: res?.email ?? "",
+                    name: res?.name ?? "",
+                    role: res?.role ?? ""
+                },
+            });
+        }
+
+      setAppLoading(false);
+    };
+
+    fetchAccount();
+
 
   }, [])
 
   return (
     <>
-      <Header />
-      <Outlet />
+    {appLoading ? 
+    <div style={{
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)'
+    }}><Spin /></div>
+    : 
+    <>
+    <Header />
+    <Outlet />
+    </>
+    }
+   
     </>
   )
 }
